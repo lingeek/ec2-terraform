50c97a4e6e97ce4cd58a6b3a32f5803b0acd8708
lingeek <raul@deva-alpine-adventure.org>
2019-11-22 10:15:27 +0200

    Adding provider

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region                  = "us-west-2"
+  shared_credentials_file = "/home/jenkins/.aws/credentials"
+  profile                 = "terraform-prod"
+}
+
 resource "aws_instance" "test-project" {
   ami = "var.centos_ami"
   instance_type = "t2.micro"
